148b2748c3ea8f79f9cba46ab78c9be158b74c50
Mart Sõmermaa <mart.somermaa@gmail.com>
2017-01-18 14:12:39 +0200

    Add JavaFx app

---
+++ src/main/java/com/github/mrts/App.java
@@ -0,0 +1,13 @@
+package com.github.mrts;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello");
+        primaryStage.show();
+    }
+
+}
---
+++ src/test/java/com/github/mrts/AppTest.java
@@ -0,0 +1,14 @@
+package com.github.mrts;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+}
